26f2d3536d43a9a859e0a26d4795a077d16c1c86
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-11-09 21:45:37 +0400

    Add Tailwind.css classes to html and body

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,14 +3,6 @@ import { LockClosedIcon } from '@heroicons/react/20/solid'
 export default function App() {
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
