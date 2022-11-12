5ad3deb2ee85a447046dcd4a0f3535b20923c397
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-11-12 20:14:17 +0400

    Create reusable PageComponent.jsx

---
+++ react/src/components/PageComponent.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+
+export default function PageComponent({title, children}) {
+  return (
+    <>
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            {title}
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          {/* Replace with your content */}
+          <div className="px-4 py-6 sm:px-0">
+            {children}
+          </div>
+          {/* /End replace */}
+        </div>
+      </main>
+    </>
+  )
+}
+
+PageComponent.propTypes = {
+  title: PropTypes.string
+}
