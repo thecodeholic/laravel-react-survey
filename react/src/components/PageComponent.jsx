9680b497d657d3c514518c1c0da468b708eab6a0
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-11-16 21:24:03 +0400

    Update PageComponent.jsx. Add support for buttons

--- react/src/components/PageComponent.jsx
+++ react/src/components/PageComponent.jsx
@@ -1,13 +1,12 @@
 import PropTypes from 'prop-types'
 
-export default function PageComponent({title, children}) {
+export default function PageComponent({title, buttons, children}) {
   return (
     <>
       <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            {title}
-          </h1>
+        <div className="flex justify-between mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">{title}</h1>
+          {buttons}
         </div>
       </header>
       <main>
