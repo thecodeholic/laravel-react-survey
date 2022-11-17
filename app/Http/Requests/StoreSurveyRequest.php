a937e02f1bf6cbbce07456832e478f63fef9da80
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-11-17 23:58:58 +0400

    Change expire_date validation rule to be after today

--- app/Http/Requests/StoreSurveyRequest.php
+++ app/Http/Requests/StoreSurveyRequest.php
@@ -40,7 +40,7 @@ class StoreSurveyRequest extends FormRequest
             'user_id' => 'exists:users,id',
             'status' => 'required|boolean',
             'description' => 'nullable|string',
-            'expire_date' => 'nullable|date|after:tomorrow',
+            'expire_date' => 'nullable|date|after:today',
             'questions' => 'array',
         ];
     }
