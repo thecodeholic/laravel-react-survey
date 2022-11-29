bc5bdbe410723f8afcd6142c3ef52b4a62ce6108
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-11-29 22:09:04 +0400

    Update survey list item

--- react/src/components/SurveyListItem.jsx
+++ react/src/components/SurveyListItem.jsx
@@ -4,32 +4,31 @@ import {ArrowTopRightOnSquareIcon, PencilIcon, TrashIcon} from "@heroicons/react
 export function SurveyListItem({survey, onClick}) {
   return (
     <div
-    className="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]"
-  >
-    <img
-      src={survey.image_url ||
-        'https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg'}
-      alt={survey.title}
-      className="w-full h-48 object-cover"
-    />
-    <h4 className="mt-4 text-lg font-bold">{survey.title}</h4>
-    <div dangerouslySetInnerHTML={{__html: survey.description}} className="overflow-hidden flex-1"></div>
+      className="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]"
+    >
+      <img
+        src={survey.image_url}
+        alt={survey.title}
+        className="w-full h-48 object-cover"
+      />
+      <h4 className="mt-4 text-lg font-bold">{survey.title}</h4>
+      <div dangerouslySetInnerHTML={{__html: survey.description}} className="overflow-hidden flex-1"></div>
 
-    <div className="flex justify-between items-center mt-3">
-      <TButton to={`surveys/${survey.id}`}>
-        <PencilIcon className="w-5 h-5 mr-2 " />
-        Edit
-      </TButton>
-      <div className="flex items-center">
-        <TButton href={`/view/survey/${survey.slug}`} circle link>
-          <ArrowTopRightOnSquareIcon className="w-5 h-5" />
+      <div className="flex justify-between items-center mt-3">
+        <TButton to={`surveys/${survey.id}`}>
+          <PencilIcon className="w-5 h-5 mr-2 "/>
+          Edit
         </TButton>
+        <div className="flex items-center">
+          <TButton href={`/view/survey/${survey.slug}`} circle link>
+            <ArrowTopRightOnSquareIcon className="w-5 h-5"/>
+          </TButton>
 
-        {survey.id && <TButton onClick={onClick} circle link color="red">
-          <TrashIcon className="w-5 h-5" />
-        </TButton>}
+          {survey.id && <TButton onClick={onClick} circle link color="red">
+            <TrashIcon className="w-5 h-5"/>
+          </TButton>}
+        </div>
       </div>
     </div>
-  </div>
   )
 }
--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -3,21 +3,45 @@ import {useStateContext} from "../contexts/ContextProvider.jsx";
 import {SurveyListItem} from "../components/SurveyListItem.jsx";
 import TButton from "../components/core/Tbutton.jsx";
 import {PlusCircleIcon} from "@heroicons/react/24/outline/index.js";
+import axiosClient from "../axios.js";
+import {useState} from "react";
+import {useEffect} from "react";
 
 const Surveys = () => {
-  const {surveys} = useStateContext()
+  const [surveys, setSurveys] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    console.log("Component mount");
+
+    setLoading(true)
+    axiosClient.get("/survey").then(({data}) => {
+      setSurveys(data.data);
+      setLoading(false)
+    });
+  }, []);
 
   return (
-    <PageComponent title='Surveys'
-                   buttons={(<TButton color="green" to="/surveys/create">
-                     <PlusCircleIcon className="w-6 h-6 mr-2" />
-                     Create new
-                   </TButton>)}>
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
-        {surveys.map(survey => (<SurveyListItem survey={survey} key={survey.id}/>))}
-      </div>
+    <PageComponent
+      title="Surveys"
+      buttons={
+        <TButton color="green" to="/surveys/create">
+          <PlusCircleIcon className="w-6 h-6 mr-2"/>
+          Create new
+        </TButton>
+      }
+    >
+      {loading ? (
+        <div className="text-center text-2xl py-3">Loading...</div>
+      ) : (
+        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+          {surveys.map((survey) => (
+            <SurveyListItem survey={survey} key={survey.id}/>
+          ))}
+        </div>
+      )}
     </PageComponent>
-  )
-}
+  );
+};
 
-export default Surveys
+export default Surveys;
