9253b69db0cd92ad8fca80bb87d39a0a932a23e1
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-02-05 20:53:58 +0400

    Small changes to dashboard

--- react/src/components/DashboardCard.jsx
+++ react/src/components/DashboardCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function DashboardCard({title, children}) {
+export default function DashboardCard({title, children, style = '', className = ''}) {
   return (
-    <div className="bg-white shadow-md p-3 text-center flex flex-col animate-fade-in-down">
+    <div className={'bg-white shadow-md p-3 text-center flex flex-col animate-fade-in-down ' + className} style={style}>
       {title && <h3 className="text-2xl font-semibold">{title}</h3>}
       {children}
     </div>
--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -32,7 +32,7 @@ export default function Dashboard() {
           <DashboardCard
             title="Total Surveys"
             className="order-1 lg:order-2"
-            style="animation-delay: 0.1s"
+            style={{ animationDelay: '0.1s' }}
           >
             <div className="text-8xl pb-4 font-semibold flex-1 flex items-center justify-center">
               {data.totalSurveys}
@@ -41,7 +41,7 @@ export default function Dashboard() {
           <DashboardCard
             title="Total Answers"
             className="order-2 lg:order-4"
-            style="animation-delay: 0.2s"
+            style={{ animationDelay: '0.2s' }}
           >
             <div className="text-8xl pb-4 font-semibold flex-1 flex items-center justify-center">
               {data.totalAnswers}
@@ -50,7 +50,7 @@ export default function Dashboard() {
           <DashboardCard
             title="Latest Survey"
             className="order-3 lg:order-1 row-span-2"
-            style="animation-delay: 0.2s"
+            style={{ animationDelay: '0.2s' }}
           >
             {data.latestSurvey && (
               <div>
@@ -103,7 +103,7 @@ export default function Dashboard() {
           <DashboardCard
             title="Latest Answers"
             className="order-4 lg:order-3 row-span-2"
-            style="animation-delay: 0.3s"
+            style={{ animationDelay: '0.3s' }}
           >
             {data.latestAnswers.length && (
               <div className="text-left">
